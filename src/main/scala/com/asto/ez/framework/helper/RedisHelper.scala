dc7e8868163b55f8317820ac2767ca2c241589a2
gudaoxuri <i@sunisle.org>
2015-12-03 17:07:20 +0800

    Add some function by redis.

--- src/main/scala/com/asto/ez/framework/helper/RedisHelper.scala
+++ src/main/scala/com/asto/ez/framework/helper/RedisHelper.scala
@@ -2,13 +2,13 @@ package com.asto.ez.framework.helper
 
 import java.lang
 
-import com.ecfront.common.Resp
+import com.ecfront.common.{JsonHelper, Resp}
 import com.typesafe.scalalogging.slf4j.LazyLogging
+import io.vertx.core.json.JsonObject
 import io.vertx.core.{AsyncResult, Handler, Vertx}
 import io.vertx.redis.op.SetOptions
 import io.vertx.redis.{RedisClient, RedisOptions}
-import scala.async.Async.{async, await}
-import scala.concurrent.ExecutionContext.Implicits.global
+
 import scala.concurrent.{Future, Promise}
 
 /**
@@ -41,7 +41,7 @@ object RedisHelper extends LazyLogging {
             }
           }
         })
-      }else{
+      } else {
         redisClient.select(db, new Handler[AsyncResult[String]] {
           override def handle(event: AsyncResult[String]): Unit = {
             if (event.succeeded()) {
@@ -55,11 +55,56 @@ object RedisHelper extends LazyLogging {
     }
   }
 
+  def custom(): RedisClient = {
+    if (useCache) {
+      redisClient
+    } else {
+      null
+    }
+  }
+
+  def exists(key: String): Future[Resp[Boolean]] = {
+    val p = Promise[Resp[Boolean]]()
+    if (useCache) {
+      redisClient.exists(key, new Handler[AsyncResult[lang.Long]] {
+        override def handle(event: AsyncResult[lang.Long]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(event.result() > 0))
+          } else {
+            logger.error(s"Redis exists error.[$key]", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis exists error.[$key]"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def get(key: String): Future[Resp[String]] = {
+    val p = Promise[Resp[String]]()
+    if (useCache) {
+      redisClient.get(key, new Handler[AsyncResult[String]] {
+        override def handle(event: AsyncResult[String]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(event.result()))
+          } else {
+            logger.error(s"Redis get error.[$key]", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis get error.[$key]"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
   /**
     * @param expire (seconds)
-    * @return
     */
-  def set(key: String, value: String, expire: Long = 0): Future[Resp[Void]] = async {
+  def set(key: String, value: String, expire: Long = 0): Future[Resp[Void]] = {
     val p = Promise[Resp[Void]]()
     if (useCache) {
       if (expire == 0) {
@@ -86,51 +131,212 @@ object RedisHelper extends LazyLogging {
         })
       }
     } else {
-      p.success(Resp.success(null))
+      p.success(Resp.notImplemented("Redis service not found."))
     }
-    await(p.future)
+    p.future
   }
 
-  def exists(key: String): Future[Resp[Boolean]] = async{
-    val p = Promise[Resp[Boolean]]()
+  def hmset(key: String, values: Map[String, String], expire: Long = 0): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
     if (useCache) {
-      redisClient.exists(key, new Handler[AsyncResult[lang.Long]] {
-        override def handle(event: AsyncResult[lang.Long]): Unit = {
+      redisClient.hmset(key, new JsonObject(JsonHelper.toJsonString(values)), new Handler[AsyncResult[lang.String]] {
+        override def handle(event: AsyncResult[lang.String]): Unit = {
           if (event.succeeded()) {
-            if (event.result() > 0) {
-              p.success(Resp.success(true))
+            if (expire != 0) {
+              redisClient.expire(key, expire.toInt, new Handler[AsyncResult[lang.Long]] {
+                override def handle(event: AsyncResult[lang.Long]): Unit = {
+                  if (event.succeeded()) {
+                    p.success(Resp.success(null))
+                  } else {
+                    logger.error(s"Redis hmset error.[$key] $values", event.cause())
+                    p.success(Resp.serverUnavailable(s"Redis hmset error.[$key] $values"))
+                  }
+                }
+              })
             } else {
-              p.success(Resp.success(false))
+              p.success(Resp.success(null))
             }
           } else {
-            logger.error(s"Redis exists error.[$key]", event.cause())
-            p.success(Resp.serverUnavailable(s"Redis exists error.[$key]"))
+            logger.error(s"Redis hmset error.[$key] $values", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hmset error.[$key] $values"))
           }
         }
       })
     } else {
-      p.success(Resp.success(false))
+      p.success(Resp.notImplemented("Redis service not found."))
     }
-    await(p.future)
+    p.future
   }
 
-  def get(key: String): Future[Resp[String]] = async {
+  def hset(key: String, field: String, value: String): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
+    if (useCache) {
+      redisClient.hset(key, field, value, new Handler[AsyncResult[lang.Long]] {
+        override def handle(event: AsyncResult[lang.Long]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(null))
+          } else {
+            logger.error(s"Redis hset error.[$key] $field $value", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hset error.[$key] $field  $value"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def hget(key: String, field: String): Future[Resp[String]] = {
     val p = Promise[Resp[String]]()
     if (useCache) {
-      redisClient.get(key, new Handler[AsyncResult[String]] {
-        override def handle(event: AsyncResult[String]): Unit = {
+      redisClient.hget(key, field, new Handler[AsyncResult[lang.String]] {
+        override def handle(event: AsyncResult[lang.String]): Unit = {
           if (event.succeeded()) {
             p.success(Resp.success(event.result()))
           } else {
-            logger.error(s"Redis get error.[$key]", event.cause())
-            p.success(Resp.serverUnavailable(s"Redis get error.[$key]"))
+            logger.error(s"Redis hget error.[$key] $field", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hget error.[$key] $field"))
           }
         }
       })
     } else {
-      p.success(Resp.success(null))
+      p.success(Resp.notImplemented("Redis service not found."))
     }
-    await(p.future)
+    p.future
+  }
+
+  def hexists(key: String, field: String): Future[Resp[Boolean]] = {
+    val p = Promise[Resp[Boolean]]()
+    if (useCache) {
+      redisClient.hexists(key, field, new Handler[AsyncResult[lang.Long]] {
+        override def handle(event: AsyncResult[lang.Long]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(event.result() > 0))
+          } else {
+            logger.error(s"Redis hexists error.[$key] $field", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hset error.[$key] $field"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def hgetall(key: String): Future[Resp[Map[String, String]]] = {
+    val p = Promise[Resp[Map[String, String]]]()
+    if (useCache) {
+      redisClient.hgetall(key, new Handler[AsyncResult[JsonObject]] {
+        override def handle(event: AsyncResult[JsonObject]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(JsonHelper.toObject(event.result().encode(), classOf[Map[String, String]])))
+          } else {
+            logger.error(s"Redis hgetall error.[$key]", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hgetall error.[$key]"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def incr(key: String, value: Any): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
+    if (useCache) {
+      value match {
+        case v if v.isInstanceOf[Long] || v.isInstanceOf[Int] =>
+          redisClient.incrby(key, v.toString.toLong, new Handler[AsyncResult[lang.Long]] {
+            override def handle(event: AsyncResult[lang.Long]): Unit = {
+              if (event.succeeded()) {
+                p.success(Resp.success(null))
+              } else {
+                logger.error(s"Redis incr error.[$key] $value", event.cause())
+                p.success(Resp.serverUnavailable(s"Redis incr error.[$key] $value"))
+              }
+            }
+          })
+        case v: Float =>
+          redisClient.incrbyfloat(key, v, new Handler[AsyncResult[String]] {
+            override def handle(event: AsyncResult[String]): Unit = {
+              if (event.succeeded()) {
+                p.success(Resp.success(null))
+              } else {
+                logger.error(s"Redis incr error.[$key] $value", event.cause())
+                p.success(Resp.serverUnavailable(s"Redis incr error.[$key] $value"))
+              }
+            }
+          })
+        case _ => p.success(Resp.badRequest(s"Redis incr error. value type [$value] not support."))
+      }
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def decr(key: String, value: Any): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
+    if (useCache) {
+      value match {
+        case v if v.isInstanceOf[Long] || v.isInstanceOf[Int] =>
+          redisClient.decrby(key, v.toString.toLong, new Handler[AsyncResult[lang.Long]] {
+            override def handle(event: AsyncResult[lang.Long]): Unit = {
+              if (event.succeeded()) {
+                p.success(Resp.success(null))
+              } else {
+                logger.error(s"Redis decr error.[$key] $value", event.cause())
+                p.success(Resp.serverUnavailable(s"Redis decr error.[$key] $value"))
+              }
+            }
+          })
+        case _ => p.success(Resp.badRequest(s"Redis decr error. value type [$value] not support."))
+      }
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def del(key: String): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
+    if (useCache) {
+      redisClient.del(key, new Handler[AsyncResult[lang.Long]] {
+        override def handle(event: AsyncResult[lang.Long]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(null))
+          } else {
+            logger.error(s"Redis del error.[$key]", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis del error.[$key]"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
+  }
+
+  def hdel(key: String, field: String): Future[Resp[Void]] = {
+    val p = Promise[Resp[Void]]()
+    if (useCache) {
+      redisClient.hdel(key, field, new Handler[AsyncResult[lang.Long]] {
+        override def handle(event: AsyncResult[lang.Long]): Unit = {
+          if (event.succeeded()) {
+            p.success(Resp.success(null))
+          } else {
+            logger.error(s"Redis hdel error.[$key]", event.cause())
+            p.success(Resp.serverUnavailable(s"Redis hdel error.[$key]"))
+          }
+        }
+      })
+    } else {
+      p.success(Resp.notImplemented("Redis service not found."))
+    }
+    p.future
   }
 
 }
--- src/test/scala/com/asto/ez/framework/function/RedisHelperSpec.scala
+++ src/test/scala/com/asto/ez/framework/function/RedisHelperSpec.scala
@@ -61,18 +61,39 @@ class RedisHelperSpec extends BasicSpec {
   }
 
   def testRedisHelperAsync() = async {
-    val exists1Resp = await(RedisHelper.exists("dop_test"))
+    await(RedisHelper.del("n_test"))
+    val exists1Resp = await(RedisHelper.exists("n_test"))
     assert(!exists1Resp.body)
-    await(RedisHelper.set("dop_test", s"""{"name":"jzy"}""", 10))
-    val exists2Resp = await(RedisHelper.exists("dop_test"))
+    await(RedisHelper.set("n_test", s"""{"name":"jzy"}""", 4))
+    val exists2Resp = await(RedisHelper.exists("n_test"))
     assert(exists2Resp.body)
-    val get2Resp = await(RedisHelper.get("dop_test"))
+    val get2Resp = await(RedisHelper.get("n_test"))
     assert(JsonHelper.toJson(get2Resp.body).get("name").asText() == "jzy")
-    Thread.sleep(10000)
-    val exists3Resp = await(RedisHelper.exists("dop_test"))
+    Thread.sleep(4000)
+    val exists3Resp = await(RedisHelper.exists("n_test"))
     assert(!exists3Resp.body)
-    val get3Resp = await(RedisHelper.get("dop_test"))
+    val get3Resp = await(RedisHelper.get("n_test"))
     assert(get3Resp.body == null)
+
+    await(RedisHelper.hmset("hash_test", Map("f1" -> "v1", "f2" -> "v2")))
+    await(RedisHelper.hset("hash_test", "f3", "v3"))
+    assert(await(RedisHelper.hget("hash_test", "f3")).body == "v3")
+    assert(await(RedisHelper.hexists("hash_test", "f3")).body)
+    val hashVals = await(RedisHelper.hgetall("hash_test")).body
+    assert(hashVals.size == 3 && hashVals("f1") == "v1" && hashVals("f2") == "v2" && hashVals("f3") == "v3")
+    await(RedisHelper.hdel("hash_test", "f3"))
+    assert(!await(RedisHelper.hexists("hash_test", "f3")).body)
+    await(RedisHelper.del("hash_test"))
+    assert(!await(RedisHelper.exists("hash_test")).body)
+
+    await(RedisHelper.del("int_test"))
+    await(RedisHelper.incr("int_test", 10))
+    await(RedisHelper.incr("int_test", 10))
+    assert(await(RedisHelper.get("int_test")).body.toLong == 20)
+    await(RedisHelper.decr("int_test", 4))
+    await(RedisHelper.decr("int_test", 2))
+    assert(await(RedisHelper.get("int_test")).body.toLong == 14)
+
   }
 
 
