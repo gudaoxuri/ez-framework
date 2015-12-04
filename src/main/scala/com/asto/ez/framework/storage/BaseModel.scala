b90f36a0d5e17751da3529b044f0916500b4cec1
gudaoxuri <i@sunisle.org>
2015-12-04 16:55:49 +0800

    .

--- src/main/scala/com/asto/ez/framework/storage/BaseModel.scala
+++ src/main/scala/com/asto/ez/framework/storage/BaseModel.scala
@@ -2,11 +2,10 @@ package com.asto.ez.framework.storage
 
 import com.asto.ez.framework.EZContext
 import com.ecfront.common.{BeanHelper, Ignore, JsonHelper, Resp}
-import com.typesafe.scalalogging.slf4j.LazyLogging
 
 import scala.concurrent.Future
 
-trait BaseModel extends Serializable with LazyLogging {
+trait BaseModel extends Serializable {
 
   @Ignore
   protected lazy val _modelClazz = this.getClass
@@ -25,15 +24,15 @@ trait BaseModel extends Serializable with LazyLogging {
 
   def getById(id: Any, context: EZContext = null): Future[Resp[this.type]]
 
-  def updateByCond(newValues: String, condition: String, parameters: List[Any]=List(), context: EZContext = null): Future[Resp[Void]]
+  def updateByCond(newValues: String, condition: String, parameters: List[Any] = List(), context: EZContext = null): Future[Resp[Void]]
 
-  def deleteByCond(condition: String, parameters: List[Any]=List(), context: EZContext = null): Future[Resp[Void]]
+  def deleteByCond(condition: String, parameters: List[Any] = List(), context: EZContext = null): Future[Resp[Void]]
 
-  def getByCond(condition: String="", parameters: List[Any]=List(), context: EZContext = null): Future[Resp[this.type]]
+  def getByCond(condition: String = "", parameters: List[Any] = List(), context: EZContext = null): Future[Resp[this.type]]
 
   def existById(id: Any, context: EZContext = null): Future[Resp[Boolean]]
 
-  def existByCond(condition: String="", parameters: List[Any]=List(), context: EZContext = null): Future[Resp[Boolean]]
+  def existByCond(condition: String = "", parameters: List[Any] = List(), context: EZContext = null): Future[Resp[Boolean]]
 
   def find(condition: String = "", parameters: List[Any] = List(), context: EZContext = null): Future[Resp[List[this.type]]]
 
