04ba5d3dc80764b34191539c3e4d022083039943
gudaoxuri <i@sunisle.org>
2016-04-29 11:27:55 +0800

    Add p2p .

--- modules/distributed/src/main/scala/com/ecfront/ez/framework/service/distributed/DTopicService.scala
+++ modules/distributed/src/main/scala/com/ecfront/ez/framework/service/distributed/DTopicService.scala
@@ -1,6 +1,10 @@
 package com.ecfront.ez.framework.service.distributed
 
+import java.util.concurrent.TimeUnit
+
+import com.ecfront.common.JsonHelper
 import com.ecfront.ez.framework.service.redis.RedisProcessor
+import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.redisson.core.{MessageListener, RTopic}
 
 /**
@@ -9,19 +13,61 @@ import org.redisson.core.{MessageListener, RTopic}
   * @param key 消息队列名
   * @tparam M 消息队列项的类型
   */
-case class DTopicService[M](key: String) {
+case class DTopicService[M](key: String) extends LazyLogging {
 
   private val topic: RTopic[M] = RedisProcessor.redis.getTopic(key)
 
+  /**
+    * 发布消息
+    *
+    * @param message 消息内容
+    */
   def publish(message: M): this.type = {
     topic.publish(message)
     this
   }
 
+  /**
+    * 订阅消息
+    *
+    * @param fun 处理函数
+    */
   def subscribe(fun: => M => Unit): this.type = {
     topic.addListener(new MessageListener[M]() {
       override def onMessage(s: String, msg: M): Unit = {
-        fun(msg)
+        try {
+          fun(msg)
+        } catch {
+          case e: Throwable =>
+            logger.error(s"Distributed subscribe [$topic] process error.", e)
+            throw e
+        }
+      }
+    })
+    this
+  }
+
+  /**
+    * 订阅消息，一条消息只由一个节点处理
+    *
+    * @param fun 处理函数
+    */
+  def subscribeOneNode(fun: => M => Unit): this.type = {
+    topic.addListener(new MessageListener[M]() {
+      override def onMessage(s: String, msg: M): Unit = {
+        val lock = RedisProcessor.redis.getAtomicLong(key + "_" + JsonHelper.toJsonString(msg).hashCode)
+        if (lock.incrementAndGet() == 1) {
+          try {
+            fun(msg)
+          } catch {
+            case e: Throwable =>
+              logger.error(s"Distributed subscribe [$topic] process error.", e)
+              throw e
+          } finally {
+            // 10天后过期，删除锁
+            lock.expire(10, TimeUnit.DAYS)
+          }
+        }
       }
     })
     this
--- modules/distributed/src/test/scala/com/ecfront/ez/framework/service/distributed/DTopicServiceSpec.scala
+++ modules/distributed/src/test/scala/com/ecfront/ez/framework/service/distributed/DTopicServiceSpec.scala
@@ -19,14 +19,14 @@ class DTopicServiceSpec extends MockStartupSpec {
 
     topic.subscribe({
       msg =>
-        println("Subscribe..." + msg.name)
+        println("Subscribe1..." + msg.name)
         result += msg.name
         c.countDown()
     })
 
     topic.subscribe({
       msg =>
-        println("Subscribe..." + msg.name)
+        println("Subscribe2..." + msg.name)
         result += msg.name
         c.countDown()
     })
@@ -48,6 +48,50 @@ class DTopicServiceSpec extends MockStartupSpec {
     c.await()
 
     assert(result.size == 4)
+    assert(result.contains("李四"))
+    assert(result.contains("张三"))
+
+  }
+
+  test("DTopic point to point 测试") {
+
+    val result = ArrayBuffer[String]()
+    val c = new CountDownLatch(2)
+
+    val topic = DTopicService[TestModel]("test_topic")
+    topic.delete()
+
+    topic.subscribeOneNode({
+      msg =>
+        println("Subscribe1..." + msg.name)
+        result += msg.name
+        c.countDown()
+    })
+
+    topic.subscribeOneNode({
+      msg =>
+        println("Subscribe2..." + msg.name)
+        result += msg.name
+        c.countDown()
+    })
+
+    val model1 = TestModel()
+    model1.name = "张三"
+    model1.bool = true
+    model1.age = 14
+    model1.id = "id001"
+    topic.publish(model1)
+
+    val model2 = TestModel()
+    model2.name = "李四"
+    model2.bool = true
+    model2.age = 14
+    model2.id = "id001"
+    topic.publish(model2)
+
+    c.await()
+
+    assert(result.size == 2)
     assert(result.contains("李四"))
     assert(result.contains("张三"))
 
