ebf69f0858d54f20009f7309a0adf5579b8c5b80
gudaoxuri <i@sunisle.org>
2016-12-17 22:17:04 +0800

    .

--- services/core/src/main/scala/com/ecfront/ez/framework/core/monitor/TaskMonitor.scala
+++ services/core/src/main/scala/com/ecfront/ez/framework/core/monitor/TaskMonitor.scala
@@ -1,10 +1,11 @@
 package com.ecfront.ez.framework.core.monitor
 
+import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.concurrent.ConcurrentHashMap
 
 import com.ecfront.ez.framework.core.EZ
-import com.ecfront.ez.framework.core.helper.TimeHelper
+import com.ecfront.ez.framework.core.helper.{TimeHelper, TimerHelper}
 import com.ecfront.ez.framework.core.logger.Logging
 
 import scala.collection.JavaConversions._
@@ -19,8 +20,18 @@ object TaskMonitor extends Logging {
     taskId
   }
 
+  def get(taskId:String):(String,Date)={
+    tasks.get(taskId)
+  }
+
+  def poll(taskId:String):(String,Date)={
+    val d=tasks.get(taskId)
+    tasks -= taskId
+    d
+  }
+
   def remove(taskId: String): Unit = {
-    tasks -= (taskId)
+    tasks -= taskId
   }
 
   def hasTask(): Boolean = {
@@ -51,5 +62,17 @@ object TaskMonitor extends Logging {
     // 再等1秒
     Thread.sleep(1000)
   }
+
+  private val yyyy_MM_dd_HH_mm_ss_SSS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS")
+  TimerHelper.periodic(60L, {
+    if (tasks.nonEmpty) {
+      val info = new StringBuffer(s"\r\n--------------Current Execute Tasks : (${tasks.size()}) --------------\r\n")
+      tasks.foreach {
+            i =>
+              info.append(s"------ ${yyyy_MM_dd_HH_mm_ss_SSS.format(i._2._2)} : [${i._1}]${i._2._1}\r\n")
+      }
+      logger.trace(info.toString)
+    }
+  })
 
 }
--- services/tpsi/src/main/scala/com/ecfront/ez/framework/service/tpsi/TPSIService.scala
+++ services/tpsi/src/main/scala/com/ecfront/ez/framework/service/tpsi/TPSIService.scala
@@ -1,18 +1,14 @@
 package com.ecfront.ez.framework.service.tpsi
 
 import java.net.SocketTimeoutException
-import java.text.SimpleDateFormat
 import java.util.Date
-import java.util.concurrent.{ConcurrentHashMap, ConcurrentMap}
 import javax.xml.ws.WebServiceException
 
 import com.ecfront.common.{JsonHelper, Resp, StandardCode}
 import com.ecfront.ez.framework.core.EZ
-import com.ecfront.ez.framework.core.helper.TimerHelper
 import com.ecfront.ez.framework.core.logger.Logging
+import com.ecfront.ez.framework.core.monitor.TaskMonitor
 import com.fasterxml.jackson.databind.JsonNode
-
-import scala.collection.JavaConversions._
 
 trait TPSIService extends Logging {
 
@@ -38,29 +34,18 @@ trait TPSIService extends Logging {
   protected def login(args: JsonNode): Resp[String] = Resp.success("")
 
   protected def execute[R](id: String, funName: String, execFun: => Any, execPostFun: Any => Resp[R], reTryTimes: Int = 0): Resp[R] = {
-    var traceId: String = ""
+    var taskId: String = ""
     try {
       logger.debug(s"[TPSI] prepare [$funName]:[${config.code}][$id]")
       limitFilter(config)
-      traceId = id + System.nanoTime()
-      if (!currEXInfo.contains(funName + "@" + config.code)) {
-        currEXInfo += funName + "@" + config.code -> new ConcurrentHashMap[String, (Date, String)]()
-      }
-      currEXInfo(funName + "@" + config.code) += traceId -> (new Date(), id)
+      taskId = TaskMonitor.add(s"[TPSI] [$funName]:[${config.code}][$id]")
       logger.info(s"[TPSI] start [$funName]:[${config.code}][$id]")
       val log = if (config.isStorage) {
         EZ_TPSI_Log.start(funName, config.code, id)
       } else null
       val execResult = execFun
       val finishTime = new Date()
-      val useTimes =
-        if (currEXInfo.contains(funName + "@" + config.code) && currEXInfo(funName + "@" + config.code).contains(traceId)) {
-          val trace = currEXInfo(funName + "@" + config.code)(traceId)
-          finishTime.getTime - trace._1.getTime
-        } else {
-          -1
-        }
-      currEXInfo(funName + "@" + config.code) -= traceId
+      val useTimes = finishTime.getTime - TaskMonitor.poll(taskId)._2.getTime
       logger.debug(s"[TPSI] finish [$funName]:[${config.code}][$id] use ${useTimes}ms , return data:" + JsonHelper.toJsonString(execResult))
       val resp = execPostFun(execResult)
       if (log != null) {
@@ -77,7 +62,7 @@ trait TPSIService extends Logging {
         case StandardCode.SERVICE_UNAVAILABLE =>
           if (reTryTimes <= 5) {
             logger.warn(s"[TPSI] service unavailable [$funName]:[${config.code}][$id],retry it [$reTryTimes]")
-            Thread.sleep(2000*reTryTimes)
+            Thread.sleep(2000 * reTryTimes)
             execute[R](id, funName, execFun, execPostFun, reTryTimes + 1)
           } else {
             logger.error(s"[TPSI] service unavailable [$funName]:[${config.code}][$id]")
@@ -98,17 +83,17 @@ trait TPSIService extends Logging {
       }
     } catch {
       case e: Throwable if e.isInstanceOf[WebServiceException] || e.isInstanceOf[SocketTimeoutException] =>
-        currEXInfo(funName + "@" + config.code) -= traceId
+        TaskMonitor.remove(taskId)
         if (reTryTimes <= 5) {
           logger.warn(s"[TPSI] timeout [$funName]:[${config.code}][$id],retry it [$reTryTimes]")
-          Thread.sleep(2000*reTryTimes)
+          Thread.sleep(2000 * reTryTimes)
           execute[R](id, funName, execFun, execPostFun, reTryTimes + 1)
         } else {
           logger.error(s"[TPSI] timeout [$funName]:[${config.code}][$id]", e)
           Resp.serverUnavailable(s"[TPSI] timeout and retry fail")
         }
       case e: Throwable =>
-        currEXInfo(funName + "@" + config.code) -= traceId
+        TaskMonitor.remove(taskId)
         logger.warn(s"[TPSI] fail [$funName]:[${config.code}][$id]", e)
         Resp.serverError(s"[TPSI] fail ${e.getMessage}")
     }
@@ -145,22 +130,5 @@ trait TPSIService extends Logging {
     }
     EZ.cache.get(FLAG_TOKEN + config.code)
   }
-
-  private val currEXInfo = collection.mutable.Map[String, ConcurrentMap[String, (Date, String)]]()
-  private val yyyy_MM_dd_HH_mm_ss_SSS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS")
-  TimerHelper.periodic(60L, {
-    val currExecCount = currEXInfo.map(_._2.size).sum
-    if (currExecCount != 0) {
-      val info = new StringBuffer(s"\r\n--------------Current exchange ($currExecCount) --------------\r\n")
-      currEXInfo.foreach {
-        map =>
-          map._2.foreach {
-            i =>
-              info.append(s"------ ${yyyy_MM_dd_HH_mm_ss_SSS.format(i._2._1)} : ${map._1}-${i._2._2}\r\n")
-          }
-      }
-      logger.info(info.toString)
-    }
-  })
 
 }
